87090e7b14326ef9c9066e306b41a98ab8bb3f74
James Taylor <jamestaylor3@protonmail.com>
2020-08-08 03:59:24 -0400

    moved model state to private vars

--- hlpy/model.py
+++ hlpy/model.py
@@ -4,33 +4,27 @@ import torch
 
 from .client import Client
 
-HIDDENLAYER_URL = 'http://localhost:3000/pub/v1'
-
-# make state module / class that handles in memory stuff and loading from memory
-cycleid = None
-neural_net = None
-optimizer = None
+HERE = os.path.dirname(os.path.realpath(__file__))
 
 class Model:
 	def __init__(self, model_id, secret_key):
-		# try to find and load a current state (model/cycleid/etc)
-
 		# make new client to the api
-		c = Client(model_id, secret_key)
+		self._client = Client(model_id, secret_key)
 
 		# verify the model_id and secret_key
-		resp = c.verify()
+		resp = self._client.verify()
 
 		if not resp:
 			print('Not a valid model')
 
-		self._client = c
 		self._model_id = model_id
 		self._secret_key = secret_key
 
-	def forward(self, input):
-		global cycleid, neural_net
+		self._cycleid = None
+		self._neural_net = None
+		self._optimizer = None
 
+	def forward(self, input):
 		# check the most recent cycleid
 		resp = self._client.get_cycleid()
 
@@ -38,55 +32,59 @@ class Model:
 			print('No cycle id yet. Model has not been deployed')
 			return None
 
-		if resp != cycleid:
+		if resp != self._cycleid:
 			# we have a new model to download
-			cycleid = resp
+			self._cycleid = resp
 			self._download_current_model()
 
-		here = os.path.dirname(os.path.realpath(__file__))
-
-		if not neural_net:
+		if not self._neural_net:
 			# load the neural net and weights from disk into memory
-			from .data.nn import Net
-			neural_net = Net()
-			neural_net.load_state_dict(torch.load(os.path.join(here, 'data', 'weights.pt')))
+			self._load_neural_network()
 
-		return neural_net.forward(input)
+		return self._neural_net.forward(input)
 
 	def optimize(self, input, target):
-		global neural_net, optimizer
-
 		# optimize the stored network
+		# send to the blockchain if it is ready
 		pass
 
-		# send to the blockchain if it is ready
-
 	def _download_current_model(self):
-		global neural_net, optimizer
-
 		# download and save the most recent network
 		resp = self._client.get_update()
 
 		update = resp['update']
 
-		here = os.path.dirname(os.path.realpath(__file__))
-
 		if 'model' in update:
-			with open(os.path.join(here, 'data', 'nn.py'), 'w') as file:
+			with open(os.path.join(HERE, 'data', 'nn.py'), 'w') as file:
 				file.write(update['model'])
 
 			# reset in memory so it will be reloaded later
-			neural_net = None
+			self._neural_net = None
 
 		if 'weights' in update:
 			# base64 is not the most efficient. need to change this
-			with open(os.path.join(here, 'data', 'weights.pt'), 'wb+') as file:
+			with open(os.path.join(HERE, 'data', 'weights.pt'), 'wb+') as file:
 				file.write(base64.b64decode(update['weights']))
 
-			neural_net = None
+			self._neural_net = None
 
 		if 'optim' in update:
-			with open(os.path.join(here, 'data', 'optim.txt'), 'w') as file:
+			with open(os.path.join(HERE, 'data', 'optim.py'), 'w') as file:
 				file.write(update['optim'])
 
-			optimizer = None
+			self._optimizer = None
+
+	def _load_state(self):
+		with open(os.path.join(HERE, 'data', 'state.json')) as file:
+			contents = json.loads(file.read())
+			self._cycleid = contents['cycleid']
+
+	def _load_neural_network(self):
+		from .data.nn import Net
+		self._neural_net = Net()
+		self._neural_net.load_state_dict(
+			torch.load(os.path.join(HERE, 'data', 'weights.pt')))
+
+	def _load_optimizer(self):
+		from .data.optim import optim
+		self._optimizer = optim
